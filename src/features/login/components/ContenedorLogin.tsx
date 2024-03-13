6f661d87466fdda483bf54510e7939c4b19bb180
Erika <erikasandoval-09@hotmail.com>
2024-03-12 23:41:00 -0300

    Se agrega Formik con Yup a ContenedorLogin

--- src/features/login/components/ContenedorLogin.tsx
+++ src/features/login/components/ContenedorLogin.tsx
@@ -1,9 +1,11 @@
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import logoImg from "../../../assets/logo.svg";
 
 import style from "./ContenedorLogin.module.css";
-import { handleChangeInput } from "@/helpers/handleTextBox";
 import { useAppDispatch } from "@/store/hooks";
 import { getUser } from "@/store/slices/auth";
 import Loading from "@/components/Loading/Loading";
@@ -12,99 +14,110 @@ import { PrimeModal } from "@/primeComponents/PrimeModal/PrimeModal";
 import { ValidationModal } from "@/features/NuevaTransaccion/ValidationModal/ValidationModal";
 
 export let ContenedorLogin = () => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const errorLogin = useModal();
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState<boolean>(false)
+	const errorLogin = useModal();
 
-  const [loading, setLoading] = useState<boolean>(false);
+	const validationSchema = Yup.object().shape({
+		user_name_or_email: Yup.string().required("El usuario es requerido"),
+		password: Yup.string().required("La contraseña es requerida"),
+	});
 
-  const [login, setLogin] = useState<any>({
-    user_name_or_email: "",
-    password: "",
-  });
+	const formik = useFormik({
+		initialValues: {
+			user_name_or_email: "",
+			password: "",
+		},
+		validationSchema: validationSchema,
+		onSubmit: async (values) => {
+			try {
+				setLoading(true);
+				await dispatch(getUser(values));
+			} catch (error) {
+				console.error("Error al iniciar sesión:", error);
+				
+			} finally {
+				setLoading(false);
+				errorLogin.onVisibleModal();
+			}
+		},
+	});
 
-  const handleLogin = async () => {
-    try {
-      setLoading(true);
-      await dispatch(getUser(login));
-    } catch (error) {
-      console.error("Error al iniciar sesión:", error);
-    } finally {
-      setLoading(false);
-      errorLogin.onVisibleModal();
-    }
-  };
-  const handleRecuperarContraseña = () => {
-    navigate("/recuperar-password");
-  };
+	const handleRecuperarContraseña = () => {
+		navigate("/recuperar-password");
+	};
 
-  return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className={style.login__container}>
-          <div className={style.logo__container}>
-            <img
-              className={style.logoImg}
-              src={logoImg}
-              alt="logo de la empresa"
-            />
-          </div>
-
-          <div className={style.loginForm__container}>
-            <p className={style.loginForm__title}>Iniciar sesión</p>
-
-            <div className={style.textInput__container}>
-              {/* <label className={style.textInput__label}>Email / Usuario</label> */}
-              <label className={style.textInput__label}>Usuario</label>
-              <input
-                className={style.textInput__input}
-                type="text"
-                name="user_name_or_email"
-                value={login.user_name_or_email}
-                onChange={(e) => handleChangeInput(e, setLogin)}
-              />
-            </div>
-
-            <div className={style.textInput__container}>
-              <label className={style.textInput__label}>Contraseña</label>
-              <input
-                className={style.textInput__input}
-                type="password"
-                name="password"
-                value={login.password}
-                onChange={(e) => handleChangeInput(e, setLogin)}
-              />
-            </div>
-
-            <p
-              className={style.recoveryPassword__text}
-              onClick={handleRecuperarContraseña}
-            >
-              Olvidaste tu contraseña?
-            </p>
-
-            <div className={style.loginForm__button__container}>
-              <button className={style.loginForm__button} onClick={handleLogin}>
-                Ingresar
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-      <PrimeModal
-        header="Error en inicio de sesión"
-        modalStatus={errorLogin.modalStatus}
-        onHideModal={errorLogin.onHideModal}
-        titleCenter
-      >
-        <ValidationModal
-          onHideModal={errorLogin.onHideModal}
-          description="Usuario o contraseña son incorrectas"
-          textButton="Volver a intentar"
-        />
-      </PrimeModal>
-    </>
-  );
+	return (
+		<>
+		  {loading ? (
+			<Loading />
+		  ) : (
+			<div className={style.login__container}>
+			<div className={style.logo__container}>
+			  <img className={style.logoImg} src={logoImg} alt="logo de la empresa" />
+			</div>
+	
+			<div className={style.loginForm__container}>
+			  <p className={style.loginForm__title}>Iniciar sesión</p>
+	
+			  <form onSubmit={formik.handleSubmit}>
+				<div className={style.textInput__container}>
+				  <label className={style.textInput__label}>Usuario</label>
+				  <input
+					className={style.textInput__input}
+					type="text"
+					name="user_name_or_email"
+					value={formik.values.user_name_or_email}
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+				  />
+				  {formik.touched.user_name_or_email && formik.errors.user_name_or_email ? (
+					<div className={style.error}>{formik.errors.user_name_or_email}</div>
+				  ) : null}
+				</div>
+	
+				<div className={style.textInput__container}>
+				  <label className={style.textInput__label}>Contraseña</label>
+				  <input
+					className={style.textInput__input}
+					type="password"
+					name="password"
+					value={formik.values.password}
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+				  />
+				  {formik.touched.password && formik.errors.password ? (
+					<div className={style.error}>{formik.errors.password}</div>
+				  ) : null}
+				</div>
+	
+				<p className={style.recoveryPassword__text} onClick={handleRecuperarContraseña}>
+				  Olvidaste tu contraseña?
+				</p>
+	
+				<div className={style.loginForm__button__container}>
+				  <button className={style.loginForm__button} type="submit">
+					Ingresar
+				  </button>
+				</div>
+			  </form>
+			</div>
+		  </div>
+		)}
+	  
+		  <PrimeModal
+			header="Error en inicio de sesión"
+			modalStatus={errorLogin.modalStatus}
+			onHideModal={errorLogin.onHideModal}
+			titleCenter
+		  >
+			<ValidationModal
+			  onHideModal={errorLogin.onHideModal}
+			  description="Usuario o contraseña son incorrectas"
+			  textButton='Volver a intentar'
+			/>
+		  </PrimeModal>
+		</>
+	  );
 };
