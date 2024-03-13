b6baf5d4663585c3524af3554a1b7b52034567c6
David Machuca <davidmachuca.dev@gmail.com>
2024-03-13 07:48:52 -0500

    update: tablero vendedor

--- src/features/tesorero/components/TableroHeader/components/HeaderFiltrados/HeaderFiltrados.tsx
+++ src/features/tesorero/components/TableroHeader/components/HeaderFiltrados/HeaderFiltrados.tsx
@@ -1,20 +1,20 @@
-import CustomModal from "@/components/CustomModal/CustomModal";
+import { useModal } from "@/hooks/useModal";
 import style from "./HeaderFiltrados.module.css";
-
 import { VscFilter } from "react-icons/vsc";
 import { RxReload } from "react-icons/rx";
-import { FiDownload } from "react-icons/fi";
-import { useModal } from "@/hooks/useModal";
-import FiltroConEstado from "./components/Modals/FiltroConEstados/FiltroConEstado";
-import FiltroFechas from "./components/Modals/FiltroFechas/FiltroFechas";
-import FiltroEmpresa from "./components/Modals/FiltroEmpresa/FiltroEmpresa";
-import FiltroClientes from "./components/Modals/FiltroClientes/FiltroClientes";
-import FiltroVendedores from "./components/Modals/FiltroVendedores/FiltroVendedores";
-import FiltroFacturas from "./components/Modals/FiltroFactura/FiltroFacturas";
-import FiltroCheque from "./components/Modals/FiltroCheque/FiltroCheque";
-import FiltroImportes from "./components/Modals/FiltroImporte/FiltroImporte";
+
+import CustomModal from "@/components/CustomModal/CustomModal";
+import FiltroConEstado from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroConEstados/FiltroConEstado";
+import FiltroFechas from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroFechas/FiltroFechas";
+import FiltroEmpresa from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroEmpresa/FiltroEmpresa";
+import FiltroClientes from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroClientes/FiltroClientes";
+// import FiltroFacturas from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroFactura/FiltroFacturas";
+// import FiltroCheque from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroCheque/FiltroCheque";
+import FiltroImportes from "@/features/tesorero/components/TableroHeader/components/HeaderFiltrados/components/Modals/FiltroImporte/FiltroImporte";
 import { formatDate } from "@/helpers/formatDate";
 import { useState } from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import { FiDownload } from "react-icons/fi";
 
 interface Props {
 	optionsFilter?: any;
@@ -32,6 +32,7 @@ export const HeaderFiltrados = ({
 	dataTransaction,
 }: Props) => {
 	const { modalStatus, onVisibleModal, onHideModal } = useModal();
+
 	const {
 		modalStatus: modalStatus2,
 		onVisibleModal: onVisibleModal2,
@@ -47,22 +48,16 @@ export const HeaderFiltrados = ({
 		onVisibleModal: onVisibleModal4,
 		onHideModal: onHideModal4,
 	} = useModal();
-	const {
-		modalStatus: modalStatus5,
-		onVisibleModal: onVisibleModal5,
-		onHideModal: onHideModal5,
-	} = useModal();
-
-	const {
-		modalStatus: modalStatus6,
-		onVisibleModal: onVisibleModal6,
-		onHideModal: onHideModal6,
-	} = useModal();
-	const {
-		modalStatus: modalStatus7,
-		onVisibleModal: onVisibleModal7,
-		onHideModal: onHideModal7,
-	} = useModal();
+	// const {
+	// 	modalStatus: modalStatus6,
+	// 	onVisibleModal: onVisibleModal6,
+	// 	onHideModal: onHideModal6,
+	// } = useModal();
+	// const {
+	// 	modalStatus: modalStatus7,
+	// 	onVisibleModal: onVisibleModal7,
+	// 	onHideModal: onHideModal7,
+	// } = useModal();
 	const {
 		modalStatus: modalStatus8,
 		onVisibleModal: onVisibleModal8,
@@ -76,6 +71,28 @@ export const HeaderFiltrados = ({
 			[name]: value,
 		});
 	};
+
+	const [filterFacturas, setFilterFacturas] = useState(null);
+	const [filterCheques, setFilterCheques] = useState(null);
+	// const [filterImporte, setFilterImporte] = useState(null);
+
+	const onFilterChangeNumber = (value: any, name: any) => {
+		// Esto es fijo para los filtrados de n factura, n cheque e importe.
+		// bill_number
+		// check_document_number
+		setOptionsFilter((prev: any) => ({
+			...prev,
+			[name]: +value,
+		}));
+	};
+
+	const onResetFilters = () => {
+		setFilterFacturas(null);
+		setFilterCheques(null);
+		// setFilterImporte(null);
+		handleResetFilters();
+	};
+
 	const [activeFilterButton, setActiveFilterButton] = useState(false);
 
 	return (
@@ -92,12 +109,12 @@ export const HeaderFiltrados = ({
 					<div
 						className={style.header__filtrados__icon}
 						style={{ transform: "scaleX(-1)" }}
-						onClick={handleResetFilters}
+						onClick={onResetFilters}
 					>
-						<RxReload />
+						<RxReload size={14} />
 					</div>
 					<div className={style.header__filtrados__icon}>
-						<FiDownload />
+						<FiDownload size={14} />
 					</div>
 				</div>
 
@@ -105,6 +122,7 @@ export const HeaderFiltrados = ({
 
 				<div className={style.header__filtrados__content}>
 					{/* First row */}
+
 					<div className={style.header__filtrados__content__item} onClick={onVisibleModal}>
 						<div className={style.header__filtrados__content__itemGroup}>
 							<p className={style.header__filtrados__text}>Filtrado con estado:</p>
@@ -141,11 +159,7 @@ export const HeaderFiltrados = ({
 					<div className={style.header__filtrados__content__item__row2}>
 						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal2}>
 							<p className={style.header__filtrados__text}>Fechas:</p>
-							<div
-								className={style.header__filtrados__documentType__item}
-								style={{ cursor: "pointer" }}
-							>
-								{/* De: 12/11/2021 A: 12/11/2023 */}
+							<div className={style.header__filtrados__documentType__item}>
 								{` ${formatDate(optionsFilter.created_at_start)} - ${formatDate(
 									optionsFilter.created_at_end
 								)} `}
@@ -153,23 +167,17 @@ export const HeaderFiltrados = ({
 						</div>
 						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal3}>
 							<p className={style.header__filtrados__text}>Empresa:</p>
-							<div
-								className={style.header__filtrados__documentType__item}
-								style={{ cursor: "pointer" }}
-							>
+							<div className={style.header__filtrados__documentType__item}>
 								{optionsFilter.empresaName ? optionsFilter.empresaName : "Todos"}
 							</div>
 						</div>
 						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal4}>
 							<p className={style.header__filtrados__text}>Cliente:</p>
-							<div
-								className={style.header__filtrados__documentType__item}
-								style={{ cursor: "pointer" }}
-							>
+							<div className={style.header__filtrados__documentType__item}>
 								{optionsFilter.clientName ? optionsFilter.clientName : "Todos"}
 							</div>
 						</div>
-						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal5}>
+						<div className={style.header__filtrados__content__itemGroup}>
 							<p className={style.header__filtrados__text}>Vendedores:</p>
 							<div
 								className={style.header__filtrados__documentType__item}
@@ -183,22 +191,36 @@ export const HeaderFiltrados = ({
 					{/* Thrid row */}
 
 					<div className={style.header__filtrados__content__item__row3}>
-						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal6}>
+						<div className={style.header__filtrados__content__itemGroup}>
 							<p className={style.header__filtrados__content__text}>N° de Factura o debito:</p>
-							<div
-								className={style.header__filtrados__documentType__item}
-								style={{ cursor: "pointer" }}
-							>
-								{optionsFilter.bill_number ? optionsFilter.bill_number : "Buscar"}
+							<div className={style.header__filtrados__documentType__item}>
+								<input
+									type="number"
+									className={style.header__input__filter}
+									placeholder="Buscar"
+									onChange={(e: any) => setFilterFacturas(e.target.value)}
+									value={filterFacturas || ""}
+								/>
+								<IoSearchSharp
+									size={15}
+									onClick={() => onFilterChangeNumber(filterFacturas, "bill_number")}
+								/>
 							</div>
 						</div>
-						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal7}>
+						<div className={style.header__filtrados__content__itemGroup}>
 							<p className={style.header__filtrados__content__text}>N° de Cheque:</p>
 							<div className={style.header__filtrados__documentType__item}>
-								{" "}
-								{optionsFilter.check_document_number
-									? optionsFilter.check_document_number
-									: "Buscar"}{" "}
+								<input
+									type="number"
+									className={style.header__input__filter}
+									placeholder="Buscar"
+									onChange={(e: any) => setFilterCheques(e.target.value)}
+									value={filterCheques || ""}
+								/>
+								<IoSearchSharp
+									size={15}
+									onClick={() => onFilterChangeNumber(filterCheques, "check_document_number")}
+								/>
 							</div>
 						</div>
 						<div className={style.header__filtrados__content__itemGroup} onClick={onVisibleModal8}>
@@ -208,9 +230,6 @@ export const HeaderFiltrados = ({
 					</div>
 				</div>
 			</div>
-
-			{/* FILTRO ESTADO */}
-
 			<CustomModal isVisible={modalStatus} onHide={onHideModal} width="45%">
 				<FiltroConEstado
 				// optionsFilter={optionsFilter}
@@ -256,31 +275,23 @@ export const HeaderFiltrados = ({
 				/>
 			</CustomModal>
 
-			<CustomModal isVisible={modalStatus5} onHide={onHideModal5} width="40%">
-				<FiltroVendedores
-					optionsFilter={optionsFilter}
-					setOptionsFilter={setOptionsFilter}
-					onHideModal={onHideModal5}
-				/>
-			</CustomModal>
-
 			{/* 3ra */}
 
-			<CustomModal isVisible={modalStatus6} onHide={onHideModal6} width="40%">
+			{/* <CustomModal isVisible={modalStatus6} onHide={onHideModal6} width="40%">
 				<FiltroFacturas
 					optionsFilter={optionsFilter}
 					handleChange={handleChange}
 					onHideModal={onHideModal6}
 				/>
-			</CustomModal>
+			</CustomModal> */}
 
-			<CustomModal isVisible={modalStatus7} onHide={onHideModal7} width="40%">
+			{/* <CustomModal isVisible={modalStatus7} onHide={onHideModal7} width="40%">
 				<FiltroCheque
 					optionsFilter={optionsFilter}
 					handleChange={handleChange}
 					onHideModal={onHideModal7}
 				/>
-			</CustomModal>
+			</CustomModal> */}
 
 			<CustomModal isVisible={modalStatus8} onHide={onHideModal8} width="40%">
 				<FiltroImportes
