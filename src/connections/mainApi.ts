518b9647ca2ed6faa1706e01f2dabb7e3f4e4b0a
Erika <erikasandoval-09@hotmail.com>
2024-03-20 14:38:10 -0300

    Cambio url

--- src/connections/mainApi.ts
+++ src/connections/mainApi.ts
@@ -1,8 +1,8 @@
 const serverBaseURL = "http://217.76.61.88:3000";
-const serverBaseURLLocal= "http://localhost:3000";
+// const serverBaseURLLocal= "http://localhost:3000";
 const testBaseURL = "http://servernick.ddns.net:6006";
 const testNickUrl = "http://hostnick.ddns.net:6006";
 
-const urls = [serverBaseURL,serverBaseURLLocal, testBaseURL, testNickUrl];
+const urls = [serverBaseURL, testBaseURL, testNickUrl];
 
-export const url = urls[1];
+export const url = urls[0];
